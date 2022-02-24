87b130e752b48f14b3ff54a97ada4e8933c0a2f2
Martin Gilles <martin.gilles@gundlach-automotive.com>
2022-02-24 20:21:48 +0100

    trim strings

--- convert.go
+++ convert.go
@@ -1,18 +1,21 @@
 package slices
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 func ParseInt(s string) (int, error) {
-	n, err := strconv.ParseInt(s, 10, 64)
+	n, err := strconv.ParseInt(strings.TrimSpace(s), 10, 64)
 	return int(n), err
 }
 
 func ParseFloat(s string) (float64, error) {
-	return strconv.ParseFloat(s, 64)
+	return strconv.ParseFloat(strings.TrimSpace(s), 64)
 }
 
 func ParseBool(s string) (bool, error) {
-	return strconv.ParseBool(s)
+	return strconv.ParseBool(strings.TrimSpace(s))
 }
 
 func Convert[S any, D any](in []S, parseFnc func(t S) (D, error)) ([]D, error) {
